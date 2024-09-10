de008dd310d196a956fe8496e8628559af5644f8
talhakhan211 <talhakhan9429@gmail.com>
2024-09-10 13:50:34 -0700

    product-card

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buyButton = document.querySelector('.buy-button');
+    
+    buyButton.addEventListener('click', () => {
+        alert('Thank you for your purchase!');
+    });
+});
